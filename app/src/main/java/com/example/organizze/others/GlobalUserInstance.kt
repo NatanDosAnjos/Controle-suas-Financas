3c73f2e2e8d2e538f8ddfb09eb398fd8c3a15ae2
Natanael <natan.ns.ns@gmail.com>
2020-11-11 22:37:25 -0300

    Conflito de merge resolvido

--- app/src/main/java/com/example/organizze/others/GlobalUserInstance.kt
+++ app/src/main/java/com/example/organizze/others/GlobalUserInstance.kt
@@ -5,16 +5,9 @@ import com.example.organizze.model.User
 class GlobalUserInstance{
     // Este é quase um singleton. Criei uma classe que representa uma instância stática da classe Usuário
     companion object {
-<<<<<<< HEAD
         private var runnable: Runnable? = null
 
-=======
->>>>>>> 9a007cc1abc74961de212cbcb56bd6174056eb0c
-        // Instância, que apesar de não ser uma constante, pode ter seu valor recuperado em todo o app
-        // Sei que não é uma boa prática deixar ele possível a alteração por qualquer classe, estou tentando achar uma solução pra isso
-        // Por hora me garanti que estou modificando este objeto em apenas um local no projeto, que é na função OnStart()
         @JvmStatic var instance = User()
-<<<<<<< HEAD
         set(value) {
             field = value
             onChangeUser(runnable)
@@ -23,11 +16,5 @@ class GlobalUserInstance{
         private fun onChangeUser(run: Runnable?) {
             run?.run()
         }
-
-        fun setOnChangeUser(runnable: Runnable) {
-            this.runnable = runnable
-        }
-=======
->>>>>>> 9a007cc1abc74961de212cbcb56bd6174056eb0c
     }
 }
